6e5bce369f2c36af32e43d44bc4d150351622800
bokoto000 <bobi_velk@abv.bg>
2018-07-17 15:05:32 +0300

    MongoFix

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 module.exports = (settings) => {
     mongoose.connect(settings.db , err =>{
+        console.log(settings.db);
         if(err){
             console.log(err);
             return ;
