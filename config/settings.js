818386b7d278fb44a9001118ec3d408287809539
bokoto000 <bobi_velk@abv.bg>
2018-07-17 03:28:52 +0300

    Mongo env var changes

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
-        port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-        ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+        port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        ip: process.env.IP|| process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
         db: getDB()
     }
 }
