13c90d3122d0607cd860847d05d1e197bee63cd5
bokoto000 <bobi_velk@abv.bg>
2018-07-17 03:56:21 +0300

    More mongo settings changes

--- config/settings.js
+++ config/settings.js
@@ -9,10 +9,26 @@ module.exports = {
 function getDB()
 {
     db_name="calendar"
-    mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+    //mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
     //take advantage of openshift env vars when available:
-    if(process.env.OPENSHIFT_MONGODB_DB_URL){
-    mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+    mongoURL =process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL  + db_name;
+    if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+        var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+            mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+            mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+            mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+            mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+            mongoUser = process.env[mongoServiceName + '_USER'];
+      
+        if (mongoHost && mongoPort && mongoDatabase) {
+          mongoURLLabel = mongoURL = 'mongodb://';
+          if (mongoUser && mongoPassword) {
+            mongoURL += mongoUser + ':' + mongoPassword + '@';
+          }
+          // Provide UI label that excludes user id and pw
+          mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+          mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+        }
     }
-    return mongodb_connection_string;
+    return mongoURL;
 }
