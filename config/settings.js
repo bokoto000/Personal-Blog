93ed39f6b99061a7bdb52a207a441817c1534c58
bokoto000 <bobi_velk@abv.bg>
2018-07-17 15:00:04 +0300

    Mongo env var changes

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,5 @@
 module.exports = {
     development: {
-        port:process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-        ip: process.env || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
         db: getDB() ||('mongodb://127.0.0.1:27017/calendar')
     }
 }
