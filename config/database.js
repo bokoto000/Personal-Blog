445cc81173dc290e3f7daf45687c6cfc552df167
bokoto000 <bobi_velk@abv.bg>
2018-07-17 14:43:08 +0300

    Small fix

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 module.exports = (settings) => {
-    mongoose.connect(settings.db,useNewUrlParser= true , err =>{
+    mongoose.connect(settings.db , err =>{
         if(err){
             console.log(err);
             return ;
--- config/settings.js
+++ config/settings.js
@@ -1,35 +1,35 @@
 module.exports = {
     development: {
-        port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-        ip: process.env.IP|| process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-        db: getDB()
+        port:process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        ip: process.env || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+        db: getDB() ||('mongodb://127.0.0.1:27017/calendar')
     }
 }
 
 function getDB()
 {
-    db_name="calendar"
-    console.log(JSON.stringify(process.env));
-    //mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
-    //take advantage of openshift env vars when available:
-    mongoURL =process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || process.env.MONGO_SERVICE_HOST  + db_name;
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
     if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-        var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-            mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-            mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-            mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-            mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-            mongoUser = process.env[mongoServiceName + '_USER'];
-      
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+        mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+        mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+        mongoUser = process.env[mongoServiceName + '_USER'];
+
         if (mongoHost && mongoPort && mongoDatabase) {
-          mongoURLLabel = mongoURL = 'mongodb://';
-          if (mongoUser && mongoPassword) {
+            mongoURLLabel = mongoURL = 'mongodb://';
+            if (mongoUser && mongoPassword) {
             mongoURL += mongoUser + ':' + mongoPassword + '@';
-          }
-          // Provide UI label that excludes user id and pw
-          mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-          mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+            }
+            // Provide UI label that excludes user id and pw
+            mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+            mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
         }
     }
+    console.log(mongoURL);
     return mongoURL;
 }
